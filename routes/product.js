f20bdd9ee6bf86bc92c4aa79e6f0b02e7eae3380
Eden Liu <eden90267@gmail.com>
2016-12-28 12:28:49 +0800

    impl post, get method from db

--- routes/product.js
+++ routes/product.js
@@ -1,17 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+var levelup = require('levelup');
+var db = levelup('./db');
+
 router.route('/')
     .get(function (req, res, next) {
         res.send('get all products info');
     })
     .post(function (req, res, next) {
-        res.send('add a new product');
+        var object = {
+            name: req.body.name,
+            type: req.body.type,
+            price: req.body.price,
+            date: new Date()
+        };
+        db.put(req.body.id, JSON.stringify(object), function (err) {
+            if (err) return console.log('Ooops!', err) // some kind of I/O error
+            res.json(object);
+        });
     });
 
 router.route('/:id')
     .get(function (req, res, next) {
-        res.send('get a product by ID');
+        db.get(req.params.id, function (err, value) {
+            if (err) return console.log('Ooops!', err) // likely the key was not found
+            res.set('Content-Type', 'application/json');
+            res.send(value);
+        });
     })
     .put(function (req, res, next) {
         res.send('modify a product by ID');
