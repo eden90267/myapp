b0b4bd4bbd868313424efd0287f0ad8c3a3b36f7
Eden Liu <eden90267@gmail.com>
2016-12-28 16:19:57 +0800

    impl put, delete method from db

--- routes/product.js
+++ routes/product.js
@@ -6,19 +6,18 @@ var db = levelup('./db');
 
 router.route('/')
     .get(function (req, res, next) {
-        res.send('get all products info');
+        var array = [];
+        db.createReadStream()
+            .on('data', function (data) {
+                array.push(JSON.parse(data.value));
+            })
+            .on('close', function () {
+                res.set('Content-Type', 'application/json');
+                res.send(array);
+            });
     })
     .post(function (req, res, next) {
-        var object = {
-            name: req.body.name,
-            type: req.body.type,
-            price: req.body.price,
-            date: new Date()
-        };
-        db.put(req.body.id, JSON.stringify(object), function (err) {
-            if (err) return console.log('Ooops!', err) // some kind of I/O error
-            res.json(object);
-        });
+        putToDb(req, res);
     });
 
 router.route('/:id')
@@ -30,10 +29,27 @@ router.route('/:id')
         });
     })
     .put(function (req, res, next) {
-        res.send('modify a product by ID');
+        putToDb(req, res);
     })
     .delete(function (req, res, next) {
-        res.send('delete a product by ID');
+        db.del(req.params.id, function (err) {
+            if (err) { // handle I/O or other error
+            }
+            res.end();
+        });
     });
+
+function putToDb(req, res) {
+    var object = {
+        name: req.body.name,
+        type: req.body.type,
+        price: req.body.price,
+        date: new Date()
+    };
+    db.put(req.body.id, JSON.stringify(object), function (err) {
+        if (err) return console.log('Ooops!', err) // some kind of I/O error
+        res.json(object);
+    });
+}
 
 module.exports = router;
